45aad99399d0f9aeaae05e7837d06149a7883bf1
Andy Maleh <andy.am@gmail.com>
2022-02-06 15:02:19 -0500

    spec: enables guessing 2 times and winning

--- spec/app/model/five_letter_word_spec.rb
+++ spec/app/model/five_letter_word_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Wordle::Model::FiveLetterWord do
   it 'enables guessing 1 time and winning' do
-    described_class.stub(:random_word).and_return('skill')
+    allow(described_class).to receive(:random_word) {'skill'}
     
     expect(subject.guesses.count).to eq(0)
     expect(subject.guess_results.count).to eq(0)
@@ -20,12 +20,38 @@ RSpec.describe Wordle::Model::FiveLetterWord do
     expect(subject.status).to eq(:win)
   end
   
+  it 'enables guessing 2 times and winning' do
+    allow(described_class).to receive(:random_word) {'skill'}
+    
+    result = subject.guess('shmek')
+    
+    expected_result = [:green, :gray, :gray, :gray, :yellow]
+    
+    expect(result).to eq(expected_result)
+    expect(subject.guesses.count).to eq(1)
+    expect(subject.guesses.last).to eq('shmek')
+    expect(subject.guess_results.count).to eq(1)
+    expect(subject.guess_results.last).to eq(expected_result)
+    expect(subject.status).to eq(:in_progress)
+    
+    result = subject.guess('skill')
+
+    expected_result = 5.times.map {:green}
+    
+    expect(result).to eq(expected_result)
+    expect(subject.guesses.count).to eq(2)
+    expect(subject.guesses.last).to eq('skill')
+    expect(subject.guess_results.count).to eq(2)
+    expect(subject.guess_results.last).to eq(expected_result)
+    expect(subject.status).to eq(:win)
+  end
+  
   it 'refreshes five letter word after game win' do
-    described_class.stub(:random_word).and_return('skill')
+    allow(described_class).to receive(:random_word) {'skill'}
     
     result = subject.guess('skill')
     
-    described_class.stub(:random_word).and_return('abbot')
+    allow(described_class).to receive(:random_word) {'abbot'}
     
     subject.refresh
     
